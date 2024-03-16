4d52baeafcf0ab304490cb0c5494db25a01e4f5e
nadbnk <85943215+nadbnk@users.noreply.github.com>
2024-03-16 11:54:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.get('/redirect', function(req, res) {
     
  
     app.listen(app.get('port'), function() {
-        console.log("Node JS app is running at http://localhost:" + app.get('port') +"/mithuntechnologies");
+        console.log("Node JS app is running at http://13.235.50.78:" + app.get('port') +"/");
       })
     
 
